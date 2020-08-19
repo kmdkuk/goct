fb72c4f97a0a9fff7ab8a20546785b2d66ac76f7
kouki <kouworld0123@gmail.com>
2020-08-19 09:06:35 +0900

    fix: linux環境でのchange_name.sh

--- scripts/change_name.sh
+++ scripts/change_name.sh
@@ -6,7 +6,7 @@ if [ "$(uname)" == 'Darwin' ]; then
   find . -type d -name .git -prune -o -type d -name scripts -prune -o -type f -name .DS_Store -prune -o -type d -name bin -prune -o -type f -print | xargs sed -i '' "s#go-cli-template#$REPO_NAME#g"
 elif [ "$(uname)" == "Linux" ]; then
   find . -type d -name .git -prune -o -type d -name scripts -prune -o -type f -print | xargs sed -i "s#kmdkuk/go-cli-template#$1#g"
-  find . -type d -name .git -prune -o -type d -name scripts -prune -o -type f -print | xargs sed -i "s#kmdkuk/go-cli-template#$REPO_NAME#g"
+  find . -type d -name .git -prune -o -type d -name scripts -prune -o -type f -print | xargs sed -i "s#go-cli-template#$REPO_NAME#g"
 else
   echo "Your platform ($(uname -a)) is not supported."
   exit 1
