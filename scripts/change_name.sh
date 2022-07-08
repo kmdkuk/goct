714eca6769ec7f652e36cb419efa4a112ee6a2c2
kouki <kouworld0123@gmail.com>
2022-07-08 23:42:20 +0900

    update script
    Signed-off-by: kouki <kouworld0123@gmail.com>

--- scripts/change_name.sh
+++ scripts/change_name.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 ARR=($(echo $1 | tr '/' ' '))
 REPO_NAME=${ARR[1]}
+if [ ${#ARR[@]} != 2 ]; then
+  echo "invalid args, please enter <your GitHub ID>/<new repository>"
+  exit 1
+fi
 if [ "$(uname)" == 'Darwin' ]; then
   find . -type d -name .git -prune -o -type d -name scripts -prune -o -type f -name .DS_Store -prune -o -type d -name bin -prune -o -type f -print | xargs sed -i '' "s#kmdkuk/go-cli-template#$1#g"
   find . -type d -name .git -prune -o -type d -name scripts -prune -o -type f -name .DS_Store -prune -o -type d -name bin -prune -o -type f -print | xargs sed -i '' "s#go-cli-template#$REPO_NAME#g"
